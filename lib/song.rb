ae9669161d66516feda53ee03cda12a0e50884ab
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-17 03:49:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,10 +28,10 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each do |genre| 
+    @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1
-      else 
+      else
         genre_count[genre] = 1
         end
       end
